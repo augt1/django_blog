3ae811b8e4bc76309fe97085751ee0cea669d0a5
Avgoustinos Tran <augustin.tran@gmail.com>
2025-07-02 19:05:26 +0300

    added model Post

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.urls import reverse
+from django.utils import timezone
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+
+User = get_user_model()
+
+
+class Status(models.TextChoices):
+    DRAFT = 'draft', 'Draft'
+    PUBLISHED = 'published', 'Published'
+    ARCHIVED = 'archived', 'Archived'
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    published = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(unique=True, max_length=100)
+    status = models.CharField(max_length=20, choices=Status.choices, default=Status.DRAFT)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Post"
+        verbose_name_plural = "Posts"
+        #TODO: think ordering and add indexes
+        ordering = ['-created_at']
+
+    def save(self, *args, **kwargs):
+        self.slug = self.title.replace(' ', '-').lower()
+        super().save(*args, **kwargs)
+    
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', kwargs={'slug': self.slug})
