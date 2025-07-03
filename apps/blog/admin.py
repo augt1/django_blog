d59dc4b5db99236adcc1d58a05e6bc4d2c623694
Avgoustinos Tran <augustin.tran@gmail.com>
2025-07-03 14:21:08 +0300

    added created_at and updated_adt in update post admin form

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -30,16 +30,25 @@ class PostAdmin(admin.ModelAdmin):
     autocomplete_fields = ["author"]
     show_facets = admin.ShowFacets.ALWAYS
 
-    fieldsets = (
-        (None, {"fields": ("title", "slug", "author", "content", "status")}),
-        (
-            "Timestamps",
-            {
-                "fields": ("published_at",),
-                "classes": ("collapse",),
-            },
-        ),
-    )
+    def get_readonly_fields(self, request, obj=None):
+        if obj:  # editing form
+            return ["created_at", "updated_at"]
+        return []
+
+    def get_fieldsets(self, request, obj=None):
+        base_fields = ["title", "author", "content", "slug", "status"]
+            
+        timestamp_fields = [ "published_at"]
+
+        if obj:  # editing form
+            timestamp_fields += ['created_at', 'updated_at']
+
+        fieldsets = (
+            (None, {"fields": base_fields}),
+            ("Timestamps", {"fields": timestamp_fields, "classes": ["collapse"]}),
+        )
+        
+        return fieldsets
 
 
 class PostInline(admin.TabularInline):
