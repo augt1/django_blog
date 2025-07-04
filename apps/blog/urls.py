83cefba3ffe8c907581e0e1ca5ba5b6564abe8a8
Avgoustinos Tran <augustin.tran@gmail.com>
2025-07-04 18:50:18 +0300

    post list paagination using HTMX

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
         views.post_detail,
         name="post_detail",
     ),
+    path("partials/post_list/", views.post_list_partial, name="post_list_partial"),
     # path("tags/<slug:slug>/", views.tag_detail, name="tag_detail"),
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,16 +1,24 @@
-from django.shortcuts import get_object_or_404, render
 from django.core.paginator import Paginator
+from django.shortcuts import get_object_or_404, render
+
 from .models import Post
 
 
 def posts_list(request):
-    published_posts = Post.published.prefetch_related("tags").all()
+   
+    return render(request, "blog/posts_list.html", {})
+
+
+def post_list_partial(request):
+    published_posts = Post.published.all()
+    # published_posts = Post.published.prefetch_related("tags").all()
 
     paginator = Paginator(published_posts, 2)
     page_number = request.GET.get("page")
+    print(f"Page number: {page_number}")
     posts = paginator.get_page(page_number)
 
-    return render(request, "blog/posts_list.html", {"posts": posts})
+    return render(request, "blog/partials/post_list_partial.html", {"posts": posts})
 
 
 def post_detail(request, year, month, day, slug):
