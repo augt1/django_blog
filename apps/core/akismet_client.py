4243f04014e2af346a5e3a40cf0dc8645311fe25
Avgoustinos Tran <augustin.tran@gmail.com>
2025-07-22 13:24:32 +0300

    submit spamham to akismet_client

--- apps/core/akismet_client.py
+++ apps/core/akismet_client.py
@@ -1,7 +1,6 @@
 import requests
 from django.conf import settings
 
-
 AKISMET_BLOG_URL = settings.AKISMET_BLOG_URL
 AKISMET_API_KEY = settings.AKISMET_API_KEY
 
@@ -13,12 +12,21 @@ class AkismetClientError(Exception):
 
 
 class AkismetClient:
+    """
+    Expected kwargs:
+        user_ip (str, required): IP address of the commenter
+        comment_content (str, optional): Content of the comment
+        comment_author (str, optional): Author's name
+        comment_author_email (str, optional): Author's email
+        comment_type (str, optional): Type of the comment
+        # https://akismet.com/developers/detailed-docs/submit-spam-missed-spam/
+    """
 
     AKISMET_VERIFY_URL = "https://rest.akismet.com/1.1/verify-key"
     AKISMET_COMMENT_CHECK = "https://rest.akismet.com/1.1/comment-check"
-    # ENdpoint for false negatives
+    # This call is for submitting comments that weren’t marked as spam but should have been.
     AKISMET_SUBMIT_SPAM = "https://rest.akismet.com/1.1/submit-spam"
-    # Endpoint for false positives
+    # This call is intended for the submission of false positives – items that were incorrectly classified as spam by Akismet.
     AKISMET_SUBMIT_HAM = "https://rest.akismet.com/1.1/submit-ham"
 
     def __init__(self, api_key=AKISMET_API_KEY, blog_url=AKISMET_BLOG_URL):
@@ -42,24 +50,9 @@ class AkismetClient:
 
         return is_valid
 
-    def comment_check(
-        self,
-        user_ip,
-        comment_content,
-        comment_author=None,
-        comment_author_email=None,
-        comment_type="comment",
-    ):
+    def comment_check(self, **kwargs):
         """Checks if a comment is spam or not"""
-        data = {
-            "api_key": self.api_key,
-            "blog": self.blog_url,
-            "user_ip": user_ip,
-            "comment_content": comment_content,
-            "comment_author": comment_author,
-            "comment_author_email": comment_author_email,
-            "comment_type": comment_type,
-        }
+        data = self._build_data_dict(**kwargs)
 
         response = requests.post(
             self.AKISMET_COMMENT_CHECK,
@@ -80,6 +73,30 @@ class AkismetClient:
 
         else:
             raise AkismetClientError(f"Unexpected response from Akismet: {body}")
-        
+
         return {"status": status, "message": message}
 
+    def submit_ham(self, **kwargs):
+        data = self._build_data_dict(**kwargs)
+
+        response = requests.post(self.AKISMET_SUBMIT_HAM, data=data)
+        if response.status_code != 200:
+            raise AkismetClientError(f"Failed to submit ham: {response.text}")
+
+    def submit_spam(self, **kwargs):
+        data = self._build_data_dict(**kwargs)
+
+        response = requests.post(self.AKISMET_SUBMIT_SPAM, data=data)
+        if response.status_code != 200:
+            raise AkismetClientError(f"Failed to submit spam: {response.text}")
+
+    def _build_data_dict(self, **kwargs):
+
+        data = {
+            "api_key": self.api_key,
+            "blog": self.blog_url,
+        }
+
+        data.update({k: v for k, v in kwargs.items() if v is not None})
+
+        return data
